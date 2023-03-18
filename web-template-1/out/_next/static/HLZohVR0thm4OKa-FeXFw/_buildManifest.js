67b67a84e4aa74321a24cd1f5d0179bbe281e315
system1 <wonderweb23@outlook.com>
2023-03-18 15:43:32 +0530

    Deploy

---
+++ web-template-1/out/_next/static/HLZohVR0thm4OKa-FeXFw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/09a94738431b7599.css","static/chunks/pages/index-442edbf49e28262b.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
